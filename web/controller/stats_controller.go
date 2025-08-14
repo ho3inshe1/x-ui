5b0f2149a8d4467d9379e502807bf73758dae18d
ho3inshe1 <ho3inshe1@gmail.com>
2025-08-14 12:09:58 +0330

    stats_controller.go

--- web/controller/stats_controller.go
+++ web/controller/stats_controller.go
@@ -1,1 +1,46 @@
 
+package controller
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+    "github.com/alireza0/x-ui/web/service"
+    "github.com/alireza0/x-ui/database/model"
+)
+
+type StatsController struct {
+    userService    service.UserService
+    settingService service.SettingService
+}
+
+func NewStatsController(userService service.UserService, settingService service.SettingService) *StatsController {
+    return &StatsController{
+        userService:    userService,
+        settingService: settingService,
+    }
+}
+
+func (c *StatsController) GetUserStats(ctx *gin.Context) {
+    token := ctx.Param("token")
+    
+    // بررسی فعال بودن قابلیت
+    if !c.settingService.GetSettings().EnableUserTracking {
+        ctx.JSON(http.StatusForbidden, gin.H{"error": "این قابلیت غیرفعال است"})
+        return
+    }
+
+    // یافتن کاربر بر اساس توکن
+    user, err := c.userService.GetUserByToken(token)
+    if err != nil {
+        ctx.JSON(http.StatusNotFound, gin.H{"error": "کاربر یافت نشد"})
+        return
+    }
+
+    // برگرداندن اطلاعات کاربر
+    ctx.JSON(http.StatusOK, gin.H{
+        "upload":   user.Up,
+        "download": user.Down,
+        "total":    user.Total,
+        "expiry":   user.Expiry,
+    })
+}
